0645461e890c2d63f55f301cb9f258c0a9137dc9
синечка ♡ <63897347+ktnk-dev@users.noreply.github.com>
2024-07-04 11:19:12 +0300

    редизайн
    - возможный фикс #2
    - шрифты изменены на SF Pro Display и Nunito
    - изменен начальный экран на более красивый
    - редизайн кнопок в полном рецепте 
    - редизайн показа минимальной информации (коллекций)

--- main.js
+++ main.js
@@ -22,12 +22,10 @@ async function init() {
         cookie.refresh()
         cookie.init()
     }
-    await sleep(200)
+    await sleep(250)
     await fetch('./collection.json').then(e => {return e.json()}).then(async d => {
         collections.data = d
-        var total_recipes = 0
-        Object.keys(d).forEach(c => {total_recipes += Object.keys(d[c].recipes).length})
-        try {document.querySelector('#inbase').innerHTML = `В базе ${total_recipes} рецептов и ${Object.keys(d).length} коллекций`} catch {}
+        if (window.location.search == '') app.back()
         document.querySelector('.load').style.opacity = 0
         await sleep(300)
         document.querySelector('.load').remove()
@@ -174,32 +172,35 @@ const collections = {
 }
 const main = {
     clear: async (direction) => {
-        if (debug) console.log('Clearing main element innerHTML')
+        if (debug) console.log('Clearing main element innerHTML:', parseFloat(direction.split(' ')[1]), 's')
         document.querySelectorAll('main > *').forEach(e => {
-            e.style.animation = `hide${direction} .25s cubic-bezier(.99,-0.02,.54,.74)`
+            e.style.animation = `hide${direction} cubic-bezier(.99,-0.02,.54,.74)`
         })
         await sleep(240)
         try{document.querySelector('main').classList.remove('default')}catch{}
         document.querySelector('main').innerHTML = ''
     },
     render: async (innerHTML, direction) => {
-        if (debug) console.log('Rendering new main element innerHTML')
-        if (direction == animations.to_left_side) {direction = animations.to_right_side}
-        else {direction = animations.to_left_side}
+        if (debug) console.log('Rendering new main element innerHTML:', parseFloat(direction.split(' ')[1]), 's')
+        if (direction == animations.from_top) {}
+        else if (direction == animations.to_left_side) {direction = animations.to_right_side}
+        else if (direction == animations.to_right_side) {direction = animations.to_left_side}
         document.querySelector('main').innerHTML = innerHTML
         document.querySelectorAll('main > *').forEach(e => {
-            e.style.animation = `hide${direction} .25s cubic-bezier(.99,-0.02,.54,.74) reverse`
+            e.style.animation = `hide${direction} cubic-bezier(.99,-0.02,.54,.74) reverse`
         })
-        await sleep(240)
+        await sleep(parseFloat(direction.split(' ')[1])*1000 - 10)
         document.querySelectorAll('main > *').forEach(e => {
             e.style.animation = ``
         })
+        rendering_active = false
     }
 }
 
 const animations = {
-    to_left_side: '',
-    to_right_side: '_r',
+    to_left_side: ' .25s',
+    to_right_side: '_r .25s',
+    from_top: '_d 1s',
 }
 
 
@@ -210,9 +211,15 @@ const generate = {
 <div class="recipe_mini" onclick="app.showRecipe('${id}')">
     <h2>${recipe.name}</h2>
     <div class="info">
-        <span>${recipe.data.calories} ккал.</span>
-        <span>${recipe.collection}</span>
-        <span>${recipe.data.type}</span>
+        <span>
+            <span>
+                <span>
+                    ${recipe.data.calories} ккал.
+                </span>
+                ${recipe.collection}
+            </span>
+            ${recipe.data.type}
+        </span>
     </div>
 </div>
         `
@@ -235,12 +242,18 @@ const generate = {
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/></svg>
         <span>Назад</span>
     </div>
-    <div class="collections">
-        <span>${recipe.data.calories} ккал.</span>
-        <span>${recipe.collection}</span>
-        <span>${recipe.data.type}</span>
-    </div>
     <h1>${recipe.name}</h1>
+    <div class="collections">
+        <span>
+            <span>
+                <span>
+                    ${recipe.data.calories} ккал.
+                </span>
+                ${recipe.collection}
+            </span>
+            ${recipe.data.type}
+        </span>
+    </div>
     <div class="info">
         <div class="ingredients">
             <h2>Ингредиенты</h2>
@@ -265,14 +278,14 @@ const generate = {
             <span>Удалить рецепт</span>`
         }
         </button>
-        <button onclick="app.copy('${id}')">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg>
-            <span>Скопировать текст</span>
-        </button>
-        <button onclick="app.share('${id}')">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M720-80q-50 0-85-35t-35-85q0-7 1-14.5t3-13.5L322-392q-17 15-38 23.5t-44 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q23 0 44 8.5t38 23.5l282-164q-2-6-3-13.5t-1-14.5q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-23 0-44-8.5T638-672L356-508q2 6 3 13.5t1 14.5q0 7-1 14.5t-3 13.5l282 164q17-15 38-23.5t44-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Zm0-640q17 0 28.5-11.5T760-760q0-17-11.5-28.5T720-800q-17 0-28.5 11.5T680-760q0 17 11.5 28.5T720-720ZM240-440q17 0 28.5-11.5T280-480q0-17-11.5-28.5T240-520q-17 0-28.5 11.5T200-480q0 17 11.5 28.5T240-440Zm480 280q17 0 28.5-11.5T760-200q0-17-11.5-28.5T720-240q-17 0-28.5 11.5T680-200q0 17 11.5 28.5T720-160Zm0-600ZM240-480Zm480 280Z"/></svg>
-            <span>Поделиться</span>
-        </button>
+        <div class="mini">
+            <button onclick="app.copy('${id}')">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg>
+            </button>
+            <button onclick="app.share('${id}')">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M720-80q-50 0-85-35t-35-85q0-7 1-14.5t3-13.5L322-392q-17 15-38 23.5t-44 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q23 0 44 8.5t38 23.5l282-164q-2-6-3-13.5t-1-14.5q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-23 0-44-8.5T638-672L356-508q2 6 3 13.5t1 14.5q0 7-1 14.5t-3 13.5l282 164q17-15 38-23.5t44-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Zm0-640q17 0 28.5-11.5T760-760q0-17-11.5-28.5T720-800q-17 0-28.5 11.5T680-760q0 17 11.5 28.5T720-720ZM240-440q17 0 28.5-11.5T280-480q0-17-11.5-28.5T240-520q-17 0-28.5 11.5T200-480q0 17 11.5 28.5T240-440Zm480 280q17 0 28.5-11.5T760-200q0-17-11.5-28.5T720-240q-17 0-28.5 11.5T680-200q0 17 11.5 28.5T720-160Zm0-600ZM240-480Zm480 280Z"/></svg>
+            </button>
+        </div>
     </div>
 </div>
     `
@@ -289,17 +302,25 @@ const app = {
         var total_recipes = 0
         Object.keys(collections.data).forEach(c => {total_recipes += Object.keys(collections.data[c].recipes).length})
 
-        const total_collections = Object.keys(collections.data).length
         return `
-        <h1>Рецепты</h1>
-        <h3>Сайт с огромным количеством рецептов, отсортированные по коллекциям</h3>
-        <h3 id="inbase">В базе ${total_recipes} рецептов и ${total_collections} коллекций</h3>
-        <h3>Выбери нужный пункт в самом низу экрана, чтобы продолжить</h3>`
+        <div class="center">
+            <h1>Рецепты</h1>
+            <h2>${total_recipes} рецептов в базе</h2>
+        </div>
+        <div class="bottom">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z"/></svg>
+            <span>Выбери пункт ниже</span>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z"/></svg>
+        </div>
+        `
     },
     back: async () => {
-        if (!current_page_handler) {current_page_handler = app.default}
-        await main.clear(animations.to_right_side)
-        await main.render(await current_page_handler(), animations.to_right_side)
+        if (!current_page_handler) {
+            await main.render(await app.default(), animations.from_top)
+        } else {
+            await main.clear(animations.to_right_side)
+            await main.render(await current_page_handler(), animations.to_right_side)
+        }
     },
 
     showRecipe: async (id) => {
